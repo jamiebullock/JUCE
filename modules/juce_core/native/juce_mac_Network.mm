87c5b8a93e08fda9fb15234ace3e3c8bf5155642
ed <eddavies95@gmail.com>
2017-01-05 16:54:00 +0000

    Fix for HTTPS POST requests with keep-alive failing on OS X versions below 10.10

--- modules/juce_core/native/juce_mac_Network.mm
+++ modules/juce_core/native/juce_mac_Network.mm
@@ -881,15 +881,23 @@ public:
         connection = nullptr;
     }
 
-    bool connect (WebInputStream::Listener* webInputListener)
+    bool connect (WebInputStream::Listener* webInputListener, int attemptNumber = 0)
     {
-        createConnection ();
+        createConnection();
         if (! connection->start (owner, webInputListener))
         {
+            // Workaround for deployment targets below 10.10 where HTTPS POST requests with keep-alive fail with the NSURLErrorNetworkConnectionLost error code.
+           #if ! (JUCE_IOS || (defined (__MAC_OS_X_VERSION_MIN_REQUIRED) && defined (__MAC_10_10) && __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_10))
+            if (attemptNumber == 0 && connection->nsUrlErrorCode == NSURLErrorNetworkConnectionLost)
+            {
+                connection = nullptr;
+                return connect (webInputListener, ++attemptNumber);
+            }
+           #endif
+
             connection = nullptr;
             return false;
         }
-
 
         if (connection != nullptr && connection->headers != nil)
         {
